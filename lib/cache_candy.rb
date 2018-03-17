11f7f5829380ac14b0bf905e3db62404729ebeea
wpzero <wpcreep@gmail.com>
2018-03-17 19:35:00 +0800

    Finish basic function

--- lib/cache_candy.rb
+++ lib/cache_candy.rb
@@ -1,5 +1,29 @@
 require "cache_candy/version"
 
 module CacheCandy
-  # Your code goes here...
+  include CandyBox
+
+  write_candy_config do
+    cache_keys []
+  end
+
+  def self.included(base)
+    base.after_save :flush_cache, if: :changed
+    base.after_destroy :flush_cache
+  end
+
+  def flush_cache
+    candy_config.cache_keys.each do |cache_key|
+      Rails.cache.delete get_cache_key(cache_key)
+    end
+  end
+
+  def get_cache_key(cache_key)
+    partition_id = cache_key[/%\{(\w*)\}/, 1]
+    if partition_id.present?
+      cache_key % { partition_id.to_sym => send(partition_id) }
+    else
+      cache_key
+    end
+  end
 end
